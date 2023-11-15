4f0683b7a75d877ab69f26ad11c90685fd0ff367
Val14zp <valery14zp@gmail.com>
2023-11-15 13:18:27 -0600

    Agregué métodos para revisar la inicialización de variables

--- TablaSimbolos.py
+++ TablaSimbolos.py
@@ -1,4 +1,5 @@
 import Variable
+import AnalizadorSemantico
 
 class TablaSimbolos:
     """Tabla de simbolos que almacena los nombres de las variables como key utilizando una funcion hash."""
@@ -11,7 +12,8 @@ class TablaSimbolos:
         self.parentesis = {'(': "(", ')': ")"}
         self.bloques = {'{': "{", '}': "}"}
         self.table = {}
-
+        self.variables_sin_declarar = {}
+        self.variable_mal_iniciaizada = {}
     @staticmethod
     def esString(s):
         if s == '':
@@ -30,6 +32,46 @@ class TablaSimbolos:
         finally:
             return False
 
+    def es_operador(self, variable):
+        """Revisa si la variable es un operador."""
+        return variable in self.matematicos or variable in self.comparacion or variable in self.parentesis or variable in self.bloques
+
+    def es_tipo_dato(self, variable):
+        """Revisa si la primer palabra de una lista es uno de los tipos de datos con los que se trabaja."""
+        return variable in self.tiposDatos
+
+    def es_reservada(self, variable):
+        """Revisa si la variable es una palabra reservada."""
+        return variable in self.reservada
+
+    def esta_declarada(self, variable):
+        """Revisa si la variable esta declarada."""
+        return self.variables_sin_declarar.get(hash(variable), None)
+
+    def revisa_declaracion_variables(self, variable):
+        """Revisa está declarada."""
+        if not self.esta_declarada(variable) and not self.buscar(variable) and not self.es_reservada(variable) and not self.es_operador(variable) and not self.esString(variable) and not self.esNumero(variable) and not self.es_flotante(variable):
+            print(f"Error: La variable '{variable}' no se encuentra declarada.")
+            self.variables_sin_declarar[hash(variable)] = variable
+            return False
+
+    def coincide_tipo_dato_con_valor(self, variable):
+        """Revisa si el tipo de dato coincide con el valor."""
+        variable = self.buscar(variable)
+        if variable.tipo == "int" and not self.esNumero(variable.valor):
+            print(f"Error: El tipo de dato '{variable.tipo}' no coincide con el valor '{variable.valor}'.")
+            return False
+        elif variable.tipo == "float" and not self.es_flotante(variable.valor):
+            print(f"Error: El tipo de dato '{variable.tipo}' no coincide con el valor '{variable.valor}'.")
+            return False
+        elif variable.tipo == "string" and not self.esString(variable.valor):
+            print(f"Error: El tipo de dato '{variable.tipo}' no coincide con el valor '{variable.valor}'.")
+            return False
+
+    def revisa_inicializacion_variables(self, variable):
+        """Revisa si la variable está inicializada."""
+        if not self.coincide_tipo_dato_con_valor(variable):
+            self.variable_mal_iniciaizada[hash(variable.nombre)] = variable
     def insertar(self, variable):
         """Inserta el objeto variable a la tabla de simbolos usando una funcion hash."""
         hash_value = hash(variable.nombre)
@@ -52,7 +94,6 @@ class TablaSimbolos:
         return palabra in self.tiposDatos
 
     def procesar_lineas(self, lista):
-        variable_sin_declarar = {}
         """Procesa las listas de palabras de cada una de las lineas leidas del archivo y si son declaraciones o parametros las inserta en la tabla de simbolos como Variables."""
         for palabras in lista:
             i = 0
@@ -74,6 +115,9 @@ class TablaSimbolos:
                             valor = None
                             """Inserta la variable en la tabla de simbolos."""
                         variable = Variable.Variable(tipo, nombre, valor)
+                        if valor is not None:
+                            self.revisa_inicializacion_variables(variable)
+
                         self.insertar(variable)
                     else:
                         """Error de sintaxis."""
@@ -82,11 +126,11 @@ class TablaSimbolos:
                 else:
                     """Revisa si ya la palabra se encontraba entre las palabras sin reservar."""
 
-                    sin_declarar = variable_sin_declarar.get(hash(palabra), None)
-                    """Se mueve a la proxima palabra."""
-                    if not self.buscar(palabra) and not sin_declarar and palabra not in self.reservada and palabra not in self.matematicos and palabra not in self.comparacion and palabra not in self.parentesis and palabra not in self.bloques and not self.esNumero(palabra) and not self.esString(palabra):
-                        print(f"Error: La variable '{palabra}' no se encuentra declarada.")
-                        variable_sin_declarar[hash(palabra)] = palabra
+
+                    #To do: revisar si al retornar se está retornando correctamente la variable
+                    if not self.revisa_declaracion_variables(palabra):
+                        if self.buscar(palabra):
+                            self.revisa_inicializacion_variables(self.buscar(palabra))
                     i += 1
 
     def buscar_variable_en_tabla_simbolos(self, nombre_variable):
