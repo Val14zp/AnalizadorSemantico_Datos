12470dceba05e976b891b067bd16f4f856d17b59
Stive_JMP <115596257+JohanStive28@users.noreply.github.com>
2023-11-19 01:02:19 -0600

    Add files via upload

---
+++ datos/datos/Archivo.py
@@ -0,0 +1,31 @@
+class Archivo:
+    """Gestor para leer archivos."""
+
+    def __init__(self, nombre_archivo):
+        self.nombre_archivo = nombre_archivo
+
+    def leer_y_dividir_lineas(self):
+        """Lee el archivo y separa cada linea en palabras."""
+        try:
+            with open(self.nombre_archivo, 'r', encoding="utf=8") as archivo:
+                lineas = archivo.readlines()
+                palabras_por_linea = []
+
+                for linea in lineas:
+                    palabras = linea.replace(',', ' ').split()
+                    palabras_por_linea.append(palabras)
+
+                return palabras_por_linea
+
+        except FileNotFoundError:
+            print(f"El archivo '{self.nombre_archivo}' no se encontró.")
+            return []
+
+    def ver_listas_palabras(self):
+        """Ver las listas de palabras de cada una de las lineas leidas del archivo."""
+        lineas_divididas = self.leer_y_dividir_lineas()
+
+        for i, palabras in enumerate(lineas_divididas):
+            print(f"Línea {i + 1}: {palabras}")
+
+    
---
+++ datos/datos/main.py
@@ -0,0 +1,33 @@
+import AnalizadorSemantico
+import TablaSimbolos
+import Archivo
+
+def main():
+    print("Este es el programa principal.")
+    symbol_table = TablaSimbolos.TablaSimbolos()
+
+    nombre_archivo = "incorrecto.txt"
+    archivo = Archivo.Archivo(nombre_archivo)
+    archivo.ver_listas_palabras()
+    symbol_table.procesar_lineas(archivo.leer_y_dividir_lineas())
+    """
+    symbol_table.buscar_variable_en_tabla_simbolos("x")
+    symbol_table.buscar_variable_en_tabla_simbolos("s")
+    symbol_table.buscar_variable_en_tabla_simbolos("funcion")
+    symbol_table.buscar_variable_en_tabla_simbolos("n")
+    symbol_table.buscar_variable_en_tabla_simbolos("v")
+    symbol_table.buscar_variable_en_tabla_simbolos("w")
+    symbol_table.buscar_variable_en_tabla_simbolos("prueba")
+
+    symbol_table.eliminar("x")
+    symbol_table.eliminar("s")
+    symbol_table.eliminar("funcion")
+    symbol_table.eliminar("n")
+    symbol_table.eliminar("v")
+    symbol_table.eliminar("w")
+    symbol_table.eliminar("prueba")
+    """
+
+
+if __name__ == "__main__":
+    main()
