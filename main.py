e9f1d5abb396da18d87886308df2284b05f812ad
Stive_JMP <johan.mora.portuguez@est.una.ac.cr>
2023-11-19 01:09:48 -0600

    Revert "Llaves funcionan"
    This reverts commit 431539ad777b9e7845ff9cdb9cbf07f370741ccb.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import AnalizadorSemantico
 
 
-<<<<<<< Updated upstream
 while True:
     print("Elija una opción")
     print("\t1 - Codigo Incorrecto (incorrecto.txt)")
@@ -17,28 +16,3 @@ while True:
         analizar.leyendoCodigo("correcto.txt")
     elif opcion == "3":
         break
-=======
-    nombre_archivo = "incorrecto.txt"
-    archivo = Archivo.Archivo(nombre_archivo)
-    archivo.ver_listas_palabras()
-    symbol_table.procesar_lineas(archivo.leer_y_dividir_lineas())
-    """
-    symbol_table.buscar_variable_en_tabla_simbolos("x")
-    symbol_table.buscar_variable_en_tabla_simbolos("s")
-    symbol_table.buscar_variable_en_tabla_simbolos("funcion")
-    symbol_table.buscar_variable_en_tabla_simbolos("n")
-    symbol_table.buscar_variable_en_tabla_simbolos("v")
-    symbol_table.buscar_variable_en_tabla_simbolos("w")
-    symbol_table.buscar_variable_en_tabla_simbolos("prueba")
-
-    symbol_table.eliminar("x")
-    symbol_table.eliminar("s")
-    symbol_table.eliminar("funcion")
-    symbol_table.eliminar("n")
-    symbol_table.eliminar("v")
-    symbol_table.eliminar("w")
-    symbol_table.eliminar("prueba")
-    """
-if __name__ == "__main__":
-    main()
->>>>>>> Stashed changes
