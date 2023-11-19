797c033a47a6e7ce772e9538820a3e352cf7fa7c
wendycarballo <wendy_carballo@ymail.com>
2023-11-19 01:23:29 -0600

    Correccion docstring e insercion

--- TablaSimbolos.py
+++ TablaSimbolos.py
@@ -1,7 +1,7 @@
 import Variable
 
 class TablaSimbolos:
-    """Tabla de simbolos que almacena los nombres de las variables como key utilizando una funcion hash."""
+    """Un diccionario que almacena los nombres de las variables como key utilizando una funcion hash."""
 
     def __init__(self):
         self.tiposDatos = {'void': "void", 'int': "int", 'float': "float", 'string': "string"}
@@ -13,20 +13,22 @@ class TablaSimbolos:
         self.ignorar = {';': ";", ',': ","}
         self.table = {}
 
-        #Debe lanzar un error si trata de insertar una variable que ya esta
-
     def insertar(self, variable):
         """
         Inserta el objeto variable a la tabla de simbolos usando una funcion hash.
         Recibe como argumento un objeto Variable.
         """
         hash_value = hash(variable.nombre)
-        self.table[hash_value] = variable
+        #Lanza error si ya se inserto la variable
+        if hash_value not in self.table:
+            self.table[hash_value] = variable
+        else:
+            raise ValueError(f"La variable ya fue declarada.")
 
     def buscar(self, nombre):
         """
         Busca un objeto utilizando su nombre como key.
-        Recibe como argumento un string que se utiliza como key.
+        Recibe como argumento un string nombre que se utiliza como key.
         Retorna un objeto Variable
         """
         hash_value = hash(nombre)
@@ -36,7 +38,7 @@ class TablaSimbolos:
     def modificar(self, nombre, nuevo_valor):
         """
         Modifica el valor de una variable en la tabla de símbolos, para eso utiliza la funcion buscar.
-        Recibe como argumento un string que se utiliza como key y el nuevo valor para modificar.
+        Recibe como argumento un string nombre que se utiliza como key y el nuevo valor para modificar.
         Retorna true en caso de que se haga la modificacion y False en caso contrario.
         """
         variable = self.buscar(nombre)
@@ -51,7 +53,7 @@ class TablaSimbolos:
     def obtener_valor(self, nombre):
         """
         Obtiene el valor de una variable en la tabla de símbolos.
-        Recibe como argumento un string que se utiliza como key.
+        Recibe como argumento un string nombre que se utiliza como key.
         Retorna el valor de la variable y None en caso contrario.
         """
         variable = self.buscar(nombre)
@@ -63,7 +65,7 @@ class TablaSimbolos:
     def obtener_tipo(self, nombre):
         """
         Obtiene el tipo de una variable en la tabla de símbolos.
-        Recibe como argumento un string y el nuevo valor para modificar.
+        Recibe como argumento un string nombre que se utiliza como key.
         Retorna el tipo de la variable y None en caso contrario.
         """
         variable = self.buscar(nombre)
@@ -116,12 +118,16 @@ class TablaSimbolos:
                         #Error de sintaxis.
                         print("Error: Falta el nombre de la variable después del tipo.")
                         break
+
                 else:
                     #Se mueve a la proxima palabra.
                     i += 1
 
     def buscar_variable_en_tabla_simbolos(self, nombre_variable):
-        """Busca una variable por nombre en la tabla de simbolos creada"""
+        """
+        Busca una variable por nombre en la tabla de simbolos creada.
+        Recibe como argumento un string nombre_variable.
+        """
         variable = self.buscar(nombre_variable)
         if variable:
             print(f"Información de la variable '{nombre_variable}':")
@@ -131,5 +137,8 @@ class TablaSimbolos:
             print(f"La variable '{nombre_variable}' no se encontró en la tabla de símbolos.")
 
     def imprimir(self, nombre_variable):
-        """Busca una variable por nombre en la tabla de simbolos creada"""
+        """
+        Busca una variable por nombre en la tabla de simbolos creada.
+        Recibe como argumento un string nombre_variable.
+        """
         print(f"Valor: {self.obtener_valor(nombre_variable)}")
