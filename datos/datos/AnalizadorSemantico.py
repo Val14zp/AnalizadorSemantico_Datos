af02d460e651e216b2fa69d161d55dafd2150dfc
Stive_JMP <johan.mora.portuguez@est.una.ac.cr>
2023-11-19 01:32:28 -0600

    VERSION DEFINITIVA A TRABAJAR

--- datos/datos/AnalizadorSemantico.py
+++
@@ -1,4 +0,0 @@
-import TablaSimbolos
-class AnalizadorSemantico:
-    def __init__(self):
-        self.pila = []
--- datos/datos/Variable.py
+++
@@ -1,7 +0,0 @@
-class Variable:
-    """Es el objeto que se inserta en la tabla de simbolos."""
-
-    def __init__(self, tipo, nombre, valor):
-        self.tipo = tipo
-        self.nombre = nombre
-        self.valor = valor
